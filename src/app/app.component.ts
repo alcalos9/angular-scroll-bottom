992e255137edd15df9112d901f710f8eb8fda6c0
alcalos9 <calderon.osorio.alejandro@gmail.com>
2022-11-17 09:47:11 -0300

    codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { ITodo, TodoService } from './todos.service';
+import { ScrollToBottomDirective } from './scroll-to-bottom.directive'
 
 @Component({
+  providers: [TodoService],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
+  @ViewChild(ScrollToBottomDirective)
+  scroll!: ScrollToBottomDirective;
+
+  todos: ITodo[] = [];
+
+  constructor(private _todoService: TodoService, private cdref: ChangeDetectorRef) { }
+
+  ngAfterContentChecked() {
+    this.cdref.detectChanges();
+    
+  }
+
+  ngOnInit() {
+    this._todoService.getTodos()
+      .then((tds) => this.todos = tds);
+  }
   title = 'angular-scroll-bottom';
 }
---
+++ src/app/todos.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+export interface ITodo {
+  id: number;
+  description: string;
+}
+
+const TODOS: ITodo[] = [
+  {
+    id: 1,
+    description: 'foo',
+  },
+  {
+    id: 2,
+    description: 'bar',
+  },
+  {
+    id: 3,
+    description: 'baz',
+  },
+  {
+    id: 4,
+    description: 'quux',
+  },
+  {
+    id: 5,
+    description: 'zip',
+  },
+  {
+    id: 6,
+    description: 'plop',
+  },
+  {
+    id: 7,
+    description: 'toto',
+  },
+  {
+    id: 8,
+    description: 'tata',
+  },
+  {
+    id: 9,
+    description: 'tutu',
+  },
+  {
+    id: 10,
+    description: 'titi',
+  },
+];
+
+@Injectable()
+export class TodoService {
+  getTodos() {
+    return Promise.resolve([...TODOS]);
+  }
+}
